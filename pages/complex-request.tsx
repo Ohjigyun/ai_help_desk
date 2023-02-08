0fb00396ba366282484be1c6fc125ff8ef60f46b
Ji Gyun Oh <epraise12@naver.com>
2023-02-09 03:46:32 +0900

    feat: complex-request footer

--- pages/complex-request.tsx
+++ pages/complex-request.tsx
@@ -1,3 +1,9 @@
+import styles from '../styles/ComplexRequest.module.css'
+
 export default function ComplexRequest() {
-    return "complex-request"
+    return (
+        <div className={styles.container}>
+            <footer className={styles.footer}>위 AI는 ChatGPT를 기반으로 답변합니다. <br />image copyright vecteezy</footer>
+        </div>
+    )
 }
